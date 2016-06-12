ac917f7b70d017c9da6aab107101c1bd5b278208
bevieboo <bevin.sng@gmail.com>
2016-06-12 14:58:35 +1000

    hamburger menu hiding successful

--- javascripts/hamburger.js
+++ javascripts/hamburger.js
@@ -2,9 +2,11 @@ $('.header-hamburger').click(() => {
   $('.header-hamburger').toggleClass('is-active');
 
   if ($('.header-hamburger').hasClass('is-active')) {
-    $('.header-menu').css("display", "block");
-    $('.header-search-wrap').insertBefore('.header-user-options-account')
+    // $('.header-menu').css("display", "block");
+    $('.header-menu').addClass('is-shown');
+    $('.header-search-wrap').insertBefore('.header-user-options-account');
   } else {
-    $('.header-menu').css("display", "none");
+    // $('.header-menu').css("display", "none");
+    $('.header-menu').removeClass('is-shown');
   }
 })
